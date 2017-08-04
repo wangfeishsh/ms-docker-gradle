6460846fb908cf27eaa19587852c836b59417b2f
nannan <nannan@wywk.cn>
2017-08-04 11:56:37 +0800

    init

---
+++ src/main/java/com/bao/MsDockerGradleApplication.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDockerGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDockerGradleApplication.class, args);
+	}
+}
